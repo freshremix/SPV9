c8e151d4535ea2a5dec0a2e70e768ecff25131f1
freshremix <90133873+freshremix@users.noreply.github.com>
2023-09-02 16:29:43 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,80 +1,101 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from pytube import YouTube, Search
 import os
-import requests
-from spotipy import Spotify
-from spotipy.oauth2 import SpotifyClientCredentials  # Import SpotifyClientCredentials
+import time
+import concurrent.futures
 from dotenv import load_dotenv
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
-import time  # Import the time module
 
-# Load environment variables from .env file
 load_dotenv()
 
-# Initialize Spotify API with Client Credentials Flow
-sp = Spotify(auth_manager=SpotifyClientCredentials(client_id=os.getenv('SPOTIPY_CLIENT_ID'),
-                                                    client_secret=os.getenv('SPOTIPY_CLIENT_SECRET')))
 
-# Initialize Telegram bot
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-updater = Updater(token=TELEGRAM_TOKEN, use_context=True)
-dispatcher = updater.dispatcher
+def download_song(song_info):
+    video_id = song_info["video_id"]
+    playlist_name = song_info["playlist_name"]
+    song_name = song_info["song_name"]
 
-# Command handler to start the bot
-def start(update: Update, context: CallbackContext):
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text="🎵 Welcome to the Spotify Downloader Bot! 🎵")
+    try:
+        yt = YouTube(f'https://www.youtube.com/watch?v={video_id}')
+        try:
+            audio = yt.streams.get_audio_only()
+            try:
+                download_file = audio.download(output_path=playlist_name)
+                file_name, ext = os.path.splitext(download_file)
+                new_file = file_name + ".mp3"
+                os.rename(download_file, new_file)
+            except Exception as download_error:
+                print(f"Error in download for '{song_name}': {download_error}")
+        except Exception as audio_error:
+            print(f"Error in audio stream for '{song_name}': {audio_error}")
+    except Exception as youtube_error:
+        print(f"Error in YouTube video ID for '{song_name}': {youtube_error}")
 
-# Message handler to search for tracks and send download links
-def search_track(update: Update, context: CallbackContext):
-    chat_id = update.effective_chat.id
-    query = update.message.text
 
-    # Search for tracks on Spotify
-    results = sp.search(q=query, type='track', limit=1)
+def search_song(song_name, artist, playlist_name):
+    try:
+        search_results = Search(f"{song_name} {artist}")
+        if search_results:
+            video_id_result = search_results.results[0].video_id
+            return {
+                "video_id": video_id_result,
+                "playlist_name": playlist_name,
+                "song_name": song_name
+            }
+        else:
+            print(f"No search results found for '{song_name}'")
+            return None
+    except Exception as search_error:
+        print(f"Error in search for '{song_name}': {search_error}")
+        return None
 
-    if results['tracks']['items']:
-        track = results['tracks']['items'][0]
-        track_name = track['name']
-        artist_name = track['artists'][0]['name']
 
-        # Create a temporary folder to store downloaded music
-        temp_folder = f"./temp_{chat_id}"
-        os.makedirs(temp_folder, exist_ok=True)
-        os.chdir(temp_folder)
+def search_and_download(song_info):
+    song_name = song_info["song_name"]
+    artist = song_info["artist"]
+    playlist_name = song_info["playlist_name"]
 
-        # Create a download link using a service that provides 320kbps MP3
-        download_link = f"https://example-download-service.com/{track_name} {artist_name} 320kbps"
+    video_id = search_song(song_name, artist, playlist_name)
+    if video_id:
+        download_song(video_id)
 
-        # Send the download link to Telegram
-        context.bot.send_message(chat_id=chat_id, text=f"🎵 Downloading {track_name} by {artist_name}...")
-        context.bot.send_message(chat_id=chat_id, text=f"🔗 Download Link: {download_link}")
 
-        # Download the music file into the temporary folder
-        response = requests.get(download_link)
-        with open(f"{track_name} - {artist_name}.mp3", 'wb') as music_file:
-            music_file.write(response.content)
+def main(playlist_link):
+    seen_song_name = set()
+    filtered_list = []
 
-        # Send the downloaded music to Telegram
-        with open(f"{track_name} - {artist_name}.mp3", 'rb') as audio_file:
-            context.bot.send_audio(chat_id=chat_id, audio=audio_file, timeout=18000)
+    # Authentication - without user
+    client_credentials_manager = SpotifyClientCredentials(client_id=os.getenv('CLIENT_ID'),
+                                                          client_secret=os.getenv('CLIENT_SECRET'))
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-        # Remove the temporary folder and its contents
-        os.chdir('..')
-        os.rmdir(temp_folder)
+    playlist_uri = playlist_link.split("/")[-1].split("?")[0]
+    playlist = sp.playlist(playlist_uri)
+    playlist_name = playlist['name']
 
-        # Add a delay of 0.3 seconds between sending each audio file
-        sent += 1
-        time.sleep(0.3)
-    else:
-        context.bot.send_message(chat_id=chat_id, text="❌ Track not found on Spotify.")
+    for item in playlist["tracks"]["items"]:
+        song_name = item["track"]["name"]
+        artist = item["track"]["artists"][0]["name"]
+        if song_name not in seen_song_name:
+            seen_song_name.add(song_name)
+            filtered_list.append({
+                "song_name": song_name,
+                "artist": artist,
+                "playlist_name": playlist_name
+            })
 
-# Add handlers to the dispatcher
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+    start_time = time.time()  # Record start time
 
-search_handler = MessageHandler(Filters.text & ~Filters.command, search_track)
-dispatcher.add_handler(search_handler)
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(search_and_download, song) for song in filtered_list]
 
-# Start the bot with a polling interval of 0.3 seconds
-updater.start_polling(poll_interval=0.3)
-updater.idle()
+    # Wait for all tasks to complete
+    concurrent.futures.wait(futures)
+
+    end_time = time.time()  # Record end time
+    execution_time = end_time - start_time
+    print(f"Execution time: {execution_time:.2f}")
+
+
+if __name__ == "__main__":
+    link = ""
+    main(link)
